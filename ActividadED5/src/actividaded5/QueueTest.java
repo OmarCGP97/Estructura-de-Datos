3a51fe364c98bcc996c1f43d1a5fc46b3b7f04ab
OMARACG\OmarAlejandro <A01373707@itesm.mx>
2017-10-09 08:20:41 -0500

    insertSort y selectSort

--- ActividadED5/src/actividaded5/QueueTest.java
+++ ActividadED5/src/actividaded5/QueueTest.java
@@ -60,6 +60,43 @@ public class QueueTest {
         int[]test1={3,6,9,12,15,18,22};
         System.out.println("Elemento esta en la posición: "+binarySearch(test1,18));
         
+        System.out.println("");
+        
+        System.out.println("Array Sort try: ");
+        int[]arr1={88,34,21,22,76,3,2,12,666};
+        System.out.println("Arreglo Original: ");
+        for(int i:arr1){
+            System.out.print(i);
+            System.out.print(", ");
+        }
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Bubble type: ");
+        int[]arr2=insertionSortB(arr1);
+        System.out.println("Arreglo despues de ordenamiento ascendente: ");
+        for(int i:arr2){
+            System.out.print(i);
+            System.out.print(", ");
+        }
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Insertion Sort: ");
+        int[]arr3=insertionSort(arr1);
+        System.out.println("Arreglo despues de ordenamiento ascendente: ");
+        for(int i:arr3){
+            System.out.print(i);
+            System.out.print(", ");
+        }
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Selection Sort: ");
+        int[]arr4=selectionSort(arr1);
+        System.out.println("Arreglo despues de ordenamiento ascendente: ");
+        for(int i:arr4){
+            System.out.print(i);
+            System.out.print(", ");
+        }
+        System.out.println("");
     }
     
     public static int linearSearch(int[] arreglo, int objetivo){
@@ -94,4 +131,52 @@ public class QueueTest {
         return -1;
     }
     
+    public static int[] insertionSortB(int[] arreglo){
+         
+        int temp;
+        for (int i = 1; i < arreglo.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(arreglo[j] < arreglo[j-1]){
+                    temp = arreglo[j];
+                    arreglo[j] = arreglo[j-1];
+                    arreglo[j-1] = temp;
+                }
+            }
+        }
+        return arreglo;
+    }
+    
+    public static int[] insertionSort(int[]arreglo){
+       for(int i=1; i<arreglo.length;i++){
+           int Key=arreglo[i];
+           int j = i-1;
+           while(j>=0 && Key < arreglo[j]){
+               int temp = arreglo[j];
+               arreglo[j]=arreglo[j+1];
+               arreglo[j+1]=temp;
+               j--;
+           }
+       }
+       return arreglo;
+    }
+    
+    public static int[] selectionSort(int[]arreglo){
+       int i,j,minValue,minIndex,temp=0;
+        for(i=0; i<arreglo.length;i++){
+           minValue = arreglo[i];
+           minIndex = i;
+           for(j=i; j<arreglo.length;j++){
+               if(arreglo[j]< minValue){
+                   minValue = arreglo[j];
+                   minIndex = j;
+               }
+           }
+           if(minValue < arreglo[i]){
+               temp=arreglo[i];
+               arreglo[i]=arreglo[minIndex];
+               arreglo[minIndex]=temp;
+           }
+        }
+        return arreglo;
+    }
 }
